639871848aca50498e0226e679a131f119f6150f
nitintilwani <70369990+nitintilwani@users.noreply.github.com>
2022-02-07 14:53:15 +0530

    Create jen_env.sh

---
+++ jen_env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+length=$(ls -al /home/nitin | grep pythonjenkins | wc -l)
+if [ $length -eq 0 ]
+then
+	sudo python3 -m venv /home/nitin/pythonjenkins
+	/home/nitin/pythonjenkins/bin/python3 /home/nitin/u.py
+
+
+
+else
+	echo "This is not a directory"
+fi
